886c41c74d049203ca32ff45fda9149f41215b81
r6820 <r.euler.dx@gmail.com>
2023-09-02 13:16:50 +0900

    fix

--- src/config/default.ts
+++ src/config/default.ts
@@ -2,7 +2,7 @@
 // const port = '5000';
 // const server = `http://localhost:${port}`;
 
-const server = 'https://mosaic-server.onrender.com';
+const server = 'https://mosaic-server.azurewebsites.net';
 
 export const SOCKET_URL = `${server}/api/io`;
 export const API_URL = `${server}/api`
