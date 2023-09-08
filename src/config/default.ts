f0f2e1d20376a7f1403d61957b51a78d052dec17
r6820 <r.euler.dx@gmail.com>
2023-09-08 21:18:31 +0900

    fix server url

--- src/config/default.ts
+++ src/config/default.ts
@@ -1,7 +1,7 @@
-const port = '8000';
-const server = `http://127.0.0.1:${port}`;
+// const port = '8000';
+// const server = `http://127.0.0.1:${port}`;
 
-// const server = 'https://mosaic-server.azurewebsites.net';
+const server = 'https://mosaic-server.azurewebsites.net';
 
 export const SOCKET_URL = `${server}/api/io`;
 export const API_URL = `${server}/api`
